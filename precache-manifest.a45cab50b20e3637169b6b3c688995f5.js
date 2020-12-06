a0358dec9752a41584cc97695f18647b4146325b
ahmednooor <m.ahmednoor7@yahoo.com>
2020-12-06 17:43:01 +0500

    Updates

---
+++ precache-manifest.a45cab50b20e3637169b6b3c688995f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f81c12413067d4cef9bfa5537645cd5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a26e494967d94971d00",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "4e91ef4880669a5d1e50",
+    "url": "./static/js/2.3a77459f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3a77459f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a26e494967d94971d00",
+    "url": "./static/js/main.76cc7e74.chunk.js"
+  },
+  {
+    "revision": "efeb3f3e3811c56999ee",
+    "url": "./static/js/runtime-main.cc9b062d.js"
+  }
+]);
